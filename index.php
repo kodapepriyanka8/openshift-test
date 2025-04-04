823a39bb9eb5b35b809d95591903d0fbdb24a953
matrix <136079348+kodapepriyanka8@users.noreply.github.com>
2025-04-04 11:40:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,1 @@
-<?php
-echo "Welcome Badal to OpenShift world!!";
-?>
+<?php echo "<body style='background:aqua; text-align:center; font-family:sans-serif; color:blue;'><h1>Welcome to OpenShift World!! <span style='color:red;'>Badal</span></h1></body>"; ?>
